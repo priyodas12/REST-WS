e28208b96bd50a632f6910a12e2fb911aa72ae61
Priyobrato Das <priyodas12@gmail.com>
2018-07-14 04:09:21 +0530

    Create TweetResource.java
    resource retrieval method added

---
+++ TweetResource.java
@@ -0,0 +1,33 @@
+package io.priyo.ApplicationService.Resource;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import io.priyo.ApplicationService.Model.Tweet;
+import io.priyo.ApplicationService.Services.TweetService;
+
+@Path("/tweet")
+public class TweetResource {
+	
+	TweetService ts=new TweetService(); 
+	
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	public List<Tweet> getTweets(){
+		
+		return ts.getAllTweetInformation();
+		
+	}
+	
+	@Path("/{messageId}")
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String findTweetById(@PathParam("messageId") String messageId) {
+		return "This is political Tweet";
+	}
+}
